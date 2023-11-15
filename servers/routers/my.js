b6f6a2eb7e7c931bd876774f9500cc8f725b720c
nokatotedo <dwikypanduaqri22@gmail.com>
2023-11-15 14:47:41 +0700

    refactor: split router

--- servers/routers/my.js
+++ servers/routers/my.js
@@ -2,17 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const Users = require('../controllers/users')
-const Kosts = require('../controllers/kosts')
-const { ownerAuth, roleAuth } = require('../middlewares/auth')
+const { roleAuth } = require('../middlewares/auth')
+const kosts = require('./my-kosts')
 
 router.get('/', Users.getMyProfiles)
 router.put('/', Users.editMyProfiles)
 router.patch('/', Users.changeMyPassword)
 
 router.use(roleAuth)
-router.post('/kosts', Kosts.createMyKost)
-router.get('/kosts', Kosts.getMyKosts)
-router.put('/kosts/:id', ownerAuth, Kosts.editMyKost)
-router.delete('/kosts/:id', ownerAuth, Kosts.deleteMyKost)
+router.use('/kosts', kosts)
 
 module.exports = router
