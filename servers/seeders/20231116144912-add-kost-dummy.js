f9174e4849c3c5e4b2fd97a3a9c22a2aba30ae1f
nokatotedo <dwikypanduaqri22@gmail.com>
2023-11-16 23:08:33 +0700

    test: add seeding to testing

---
+++ servers/seeders/20231116144912-add-kost-dummy.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Kosts', [
+      {
+        name: 'Ruangan A',
+        address: 'Jl. Tanah Kusir 4',
+        description: 'Kost mantap!!!',
+        ownerId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Kosts', null, {})
+  }
+};
